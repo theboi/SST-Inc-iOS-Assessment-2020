3149bdca352a395e3eb9a85eafe2dddd5d794b08
Ryan The <ryan.the.2006@gmail.com>
2020-10-31 14:35:20 +0800

    Add A5 and part C

---
+++ SST-Inc-iOS-Assessment-2020/Chapters/SectionA.playgroundchapter/Pages/A5.playgroundpage/main.swift
@@ -0,0 +1,37 @@
+//#-hidden-code
+import TestChecker
+Test.log("a5")
+//#-end-hidden-code
+//#-hidden-code
+import UIKit
+import PlaygroundSupport
+
+let imageView = UIImageView()
+//#-end-hidden-code
+//: # A5. A Thousand Words • [3m]
+//:
+//: ```
+//: let imageView = UIImageView()
+//: ```
+//: 1. Given an image view, `imageView`, and an image called `wheres_waldo` in `Assets.xcassets`, display the image. `[1m]`
+//#-editable-code
+
+//#-end-editable-code
+
+//: 2. Adjust the scaling of the image such that the entire image can be viewed while keeping the aspect ratio (not stretched/squashed). `[1m]`
+//#-editable-code
+
+//#-end-editable-code
+
+//: 3. What is the difference between `UIImageView` and `UIImage`? Why cannot we use them interchangeably? `[1m]`
+//#-hidden-code
+let imageViewVsImage =
+//#-end-hidden-code
+//#-editable-code
+"""
+Answer:
+"""
+//#-end-editable-code
+//#-hidden-code
+PlaygroundPage.current.liveView = imageView
+//#-end-hidden-code
---
+++ SST-Inc-iOS-Assessment-2020/Chapters/SectionC.playgroundchapter/Pages/C1.playgroundpage/main.swift
@@ -0,0 +1,6 @@
+//#-hidden-code
+import TestChecker
+Test.log("c1")
+//#-end-hidden-code
+//: # C1. Quiz App • [10m]
+//:
