37bd3c6a1c8553d4a86f219a61e0ff4060c49e37
Ryan The <ryan.the.2006@gmail.com>
2020-11-06 18:22:24 +0800

    Minor changes to qns for consistency

--- SST-Inc-iOS-Assessment-2020/Chapters/SectionB.playgroundchapter/Pages/B1.playgroundpage/main.swift
+++ SST-Inc-iOS-Assessment-2020/Chapters/SectionB.playgroundchapter/Pages/B1.playgroundpage/main.swift
@@ -2,29 +2,20 @@
 import TestChecker
 Test.log("b1")
 //#-end-hidden-code
-//: # B1. School Gate • [12m]
-//: Joe has been tasked to write algorithms for a new school gate. To do so, he has to use variables and constants to store values.
+//: # B1. School Gate • [10m]
+//: Joe has been tasked to write algorithms for a new school gate. To do so, he is given several inputs as constants.
 //:
-//: 1. What is the difference between variables and constants? `[2m]`
-//#-hidden-code
-let variableVsConstant =
-//#-end-hidden-code
-//#-editable-code
-"""
-Answer: 
-"""
-//#-end-editable-code
-
 //: The school gate should unlock when **any** of the following conditions are met:
 //:
 //: * It is within operating hours **and** a student taps their pass.
 //: * A teacher taps their pass.
 //: * There is a fire.
 //:
-//: The following assumptions can also be made:
-//:
-//: * It is impossible for a person to have both a Student pass and a Teacher pass.
-//: * The variable names describe the situation at a given moment.
+//: > He may use the following assumptions and guidelines to aid him:
+//: > * It is impossible for a person to have both a Student pass and a Teacher pass.
+//: > * The variable names describe the situation at a given moment.
+//: > * Setting `isUnlocked` to `true` unlocks the gate.
+//: > * To test his code, he may change the values of the constants below, referring to the test cases provided.
 //:
 //: ```
 //: +------------------------------+----------+
@@ -52,11 +43,7 @@ Answer:
 //: +------------------------------+----------+
 //: ```
 //:
-//: > To unlock the gate, set `isUnlocked` to `true`.
-//: >
-//: > To test your code, you may change the values of the constants below, referring to the test cases provided.
-//:
-//: 2. Given the variables above, write a set of conditions that tell the gate whether or not to unlock. `[5m]`
+//: 1. Given the details above, write a set of conditions that tell the gate whether or not to unlock. `[5m]`
 //#-editable-code
 let withinOperatingHours = false
 let isStudentPass = false
@@ -68,7 +55,7 @@ var isUnlocked = false
 
 //: To determine if a given time is within operating hours, the school gate also requires an internal timer that tracks seconds elapsed since beginning of day.
 //:
-//: 3. Assuming the day starts when the program runs, write a program to keep track of the time elapsed, printing the value every second. `[5m]`
+//: 2. Assuming the day starts when the program runs, write a program to keep track of the time elapsed, printing the value every second. `[5m]`
 //#-editable-code
 
 //#-end-editable-code
