c1bb36c67c2be42629a32e6b283f2941e7a92172
Ryan The <ryan.the.2006@gmail.com>
2020-11-07 08:32:43 +0800

    Update Sketch link

--- SST-Inc-iOS-Assessment-2020/Chapters/SectionB.playgroundchapter/Pages/B4.playgroundpage/main.swift
+++ SST-Inc-iOS-Assessment-2020/Chapters/SectionB.playgroundchapter/Pages/B4.playgroundpage/main.swift
@@ -1,13 +1,8 @@
-//#-hidden-code
-import TestChecker
-Test.log("b4")
-//#-end-hidden-code
-//: # B4. RickTube • [10m]
+//: # B4. RickTube • [18m]
 //:
 //: Rick Astley wants to create a new app for his fans to watch his top hits. You have been tasked to create the user interface based on some design specifications.
 //:
-//: 1. Create a new iOS App (use Swift and Storyboard) with `Xcode.app`. Save it in the directory you previously downloaded. `[1m]`
-//: 2. Open `Main.storyboard` and create the user interface based on the specifications found [here](https://www.sketch.com/s/a3ba55be-5987-475a-a727-68722bc8d8f1/a/onvVW5). `[9m]`
+//: 1. Create a new iOS App (use Swift and Storyboard) with `Xcode.app`. Save it in the directory you previously downloaded. Open `Main.storyboard` and create the user interface based on the specifications found [here](https://www.sketch.com/s/a3ba55be-5987-475a-a727-68722bc8d8f1/a/onvVW5). `[18m]`
 //:
 //: > As much as possible, make use of Apple's UI Elements (that you have learnt).
 //: > Ensure your storyboards work on different iPhone screen sizes.
