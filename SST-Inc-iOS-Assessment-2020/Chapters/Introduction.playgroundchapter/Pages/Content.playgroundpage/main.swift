137e8f6eb47976cb2a4abb3ae1b253757f252e96
Ryan The <ryan.the.2006@gmail.com>
2020-11-19 21:29:13 +0800

    Add ans sheet and update max score

--- SST-Inc-iOS-Assessment-2020/Chapters/Introduction.playgroundchapter/Pages/Content.playgroundpage/main.swift
+++ SST-Inc-iOS-Assessment-2020/Chapters/Introduction.playgroundchapter/Pages/Content.playgroundpage/main.swift
@@ -1,6 +1,6 @@
-//: 
+//:
 //: ## Section A
-//: * A1. Variables and Constants `[3m]`
+//: * A1. Variables and Constants `[5m]`
 //: * A2. Manipulation of Values `[3m]`
 //: * A3. Collections and Objects `[6m]`
 //: * A4. FizzBuzz `[6m]`
@@ -10,4 +10,6 @@
 //: * B1. School Gate `[10m]`
 //: * B2. Leap Years `[9m]`
 //: * B3. iClicker Pro Max Plus Ultra 5GS+ `[9m]`
-//: * B4. Quiz App `[11m]`
+//: * B4. Quiz App `[19m]`
+//:
+//: Total Marks: 70
