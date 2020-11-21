170033a30a22c82c8dd10fcf4193d9c924f81367
Ryan The <ryan.the.2006@gmail.com>
2020-11-22 01:03:59 +0800

    Update readme answer sheet

--- SST-Inc-iOS-Assessment-2020/Chapters/SectionB.playgroundchapter/Pages/B3.playgroundpage/main.swift
+++ SST-Inc-iOS-Assessment-2020/Chapters/SectionB.playgroundchapter/Pages/B3.playgroundpage/main.swift
@@ -39,10 +39,10 @@ button.contentEdgeInsets = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20
 //: 2. Border radius of the button is to be set to `15`. `[1m]`
 //: 3. Background color of the button should change to a random color each time the button is pressed. `[2m]`
 //: 4. Label should display the number of times the button has been clicked whenever the button is tapped. `[1m]`
-//: 5. Every `17` clicks,
+//: 5. Every 17 clicks,
 //:     * Label should be set to the time in seconds since the first click, e.g. "30s". `[2m]`
 //:     * Text on the button is to be set to `"Yay"` (Hint: The correct answer requires setting text for the `.normal` state). `[1m]`
-//:     * Reset the text on the button back to +1 after the next click. [1m]
+//:     * Reset the text on the button back to +1 after the next click. `[1m]`
 //:
 //: Program a clicker which complies with the requirements, else, unemployment rate 📈...
 //: > The constants holding the UI elements as seen on the right can be accessed via `button` and `label`.
